b24180faf6d259f305d748b8e3082860c32fa734
alirezatabibpoor <alidizajit83@gmail.com>
2026-08-02 11:19:27 +0330

    update logo

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,38 @@
-"use client"
 import { ToastContainer } from "react-toastify";
 import "leaflet/dist/leaflet.css";
 import StoreProvider from "./storeprovider";
 import "react-toastify/dist/ReactToastify.css";
-import "./design.css"
-import {ThemeProvider} from "next-themes";
+import "./design.css";
+import { ThemeProvider } from "next-themes";
+
+export const metadata = {
+  title: "Weather App",
+  description: "Modern Weather Application",
+  icons: {
+    icon: "🌤️",
+  },
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <ThemeProvider attribute={'class'} defaultTheme='system' enableSystem>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+      >
         <body>
-        <StoreProvider>{children}</StoreProvider>
-        <ToastContainer autoClose={3000} draggable  position="bottom-center" />
-      </body>
+          <StoreProvider>
+            {children}
+          </StoreProvider>
+
+          <ToastContainer
+            autoClose={3000}
+            draggable
+            position="bottom-center"
+          />
+        </body>
       </ThemeProvider>
-     
     </html>
   );
 }
